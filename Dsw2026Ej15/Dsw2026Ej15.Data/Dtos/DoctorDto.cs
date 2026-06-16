4c0f300e1ae03f12cd60ee43cd1a7af3ee35b195
Sebastian Ferrari Osado <OsadoSebastianFerrari@alu.frt.utn.edu.ar>
2026-06-16 13:18:47 -0300

    Arreglo estructura

--- Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/DoctorDto.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/DoctorDto.cs
@@ -7,8 +7,8 @@ namespace Dsw2026Ej15.Data.Dtos
 {
     internal class DoctorDto
     {
-        public string Name { get; set; }
-        public string LicenseNumber { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LicenseNumber { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         public Guid SpecialityId { get; set; }
         public Guid Id { get; set; }
--- Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/SpecialityDto.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/SpecialityDto.cs
@@ -7,7 +7,7 @@ namespace Dsw2026Ej15.Data.Dtos
     internal class SpecialityDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
